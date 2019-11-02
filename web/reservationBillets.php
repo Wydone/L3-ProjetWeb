b621a5859448a3e2f984318b5bff653bde2903ae
Clément Coulon <cle.coulon@gmail.com>
2019-11-02 11:50:59 +0100

    Add reservation page

---
+++ web/reservationBillets.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="css/styleDefault.css" rel="stylesheet" type="text/css">
+    <link href="css/style2.css" rel="stylesheet" type="text/css">
+
+    <title>Theatres de Bourdon</title>
+</head>
+<body>
+    <div class="bandeau">
+        <h1>Festival Théâtres de Bourdon : Réservation de billets</h1>
+    </div>
+
+    <nav class="nav-top">
+		<div class="menu">
+			<ul>
+				<li>
+					<a href="accueil.php">Accueil</a>
+				</li>
+				<li>
+					<a href="programmationJourParJour.php">Jour par Jour</a>
+				</li>
+				<li>
+					<a href="programmationLieuParLieu.php">Lieu par Lieu</a>
+				</li>
+				<li>
+					<a href="programmationTroupeParTroupe.php">Troupe par Troupe</a>
+				</li>
+				<li>
+					<a href="quiSommesNous.php">Qui sommes nous</a>
+				</li>
+				<li>
+					<a href="tarifs.php">Tarif</a>
+				</li>
+	    	</ul>
+		</div>
+	</nav>
+
+    <main>
+        <div class="decalage">
+            <form name="reservationBillet" action="addReservationIntoFile.php" method="POST">
+
+                <h3>Choix du spectacle</h3>
+
+                
+
+                <input type="submit" value="Valider" />
+            </form>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
